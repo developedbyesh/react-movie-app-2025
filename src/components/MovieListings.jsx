0ff69b388f3ed8a2bfc8142d0caa3eee18a17af3
Imesh Gunasekara <imesh@jrcsoftware.com>
2025-02-02 08:43:00 +0800

    get movies from json server

--- src/components/MovieListings.jsx
+++ src/components/MovieListings.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MovieListing from './MovieListing';
-import movies from '../movies.json';
+// import movies from '../movies.json';
 
 const MovieListings = ({ isHome = false }) => {
-  const popularMovies = isHome ? movies.slice(0, 3) : movies;
+  // const popularMovies = isHome ? movies.slice(0, 3) : movies;
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const res = await fetch('http://localhost:5000/movies');
+        const data = await res.json();
+        console.log('movies api', data);
+        setMovies(data);
+      } catch (error) {
+        console.log('Error fetching data', error);
+      }
+    };
+    fetchMovies();
+  }, []);
   return (
     <section className="bg-black px-4 py-10">
       <div className="container-xl lg:container m-auto">
@@ -11,7 +27,7 @@ const MovieListings = ({ isHome = false }) => {
           Browse Movies
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {popularMovies.map((movie) => (
+          {movies.map((movie) => (
             <MovieListing key={movie.id} movie={movie} />
           ))}
         </div>
